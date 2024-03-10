8d7c777c57ac24ece5d7217fb4594ed12cd97e38
Alvin Joshua <alvinjoshua93@gmail.com>
2024-03-10 17:07:03 +0700

    metadata changes

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,5 +1,10 @@
 'use client'
 import React, { useState, useEffect } from "react";
+import { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'App',
+}
 
 interface Product {
   category: string;
